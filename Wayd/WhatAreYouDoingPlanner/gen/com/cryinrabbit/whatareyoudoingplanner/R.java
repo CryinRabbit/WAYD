6d9cdb79dca9164d45f0ff6598f66d6788e70899
gotero <gotero89@gmail.com>
2014-05-04 19:30:12 -0700

    Added Calendar Daily View (Needs a lot more work, colors, etc) Added Table Layout which will hold list of events for that day

--- Wayd/WhatAreYouDoingPlanner/gen/com/cryinrabbit/whatareyoudoingplanner/R.java
+++ Wayd/WhatAreYouDoingPlanner/gen/com/cryinrabbit/whatareyoudoingplanner/R.java
@@ -25,8 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int dummyRow=0x7f080003;
+        public static final int dummyRow2=0x7f080005;
+        public static final int dummy_event_button=0x7f080004;
+        public static final int dummy_event_button2=0x7f080006;
+        public static final int fragmentContainer=0x7f080000;
+        public static final int waydCalendar=0x7f080001;
+        public static final int waydTaskTable=0x7f080002;
     }
     public static final class layout {
         public static final int activity_wayd=0x7f030000;
@@ -36,9 +42,9 @@ public final class R {
         public static final int wayd=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int event_button=0x7f050002;
     }
     public static final class style {
         /** 
--- Wayd/WhatAreYouDoingPlanner/src/com/cryinrabbit/whatareyoudoingplanner/WaydActivity.java
+++ Wayd/WhatAreYouDoingPlanner/src/com/cryinrabbit/whatareyoudoingplanner/WaydActivity.java
@@ -1,15 +1,14 @@
 package com.cryinrabbit.whatareyoudoingplanner;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class WaydActivity extends Activity {
 
@@ -18,10 +17,14 @@ public class WaydActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wayd);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if(fragment == null) {
+			fragment = new WaydFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
 		}
+		
 	}
 
 	@Override

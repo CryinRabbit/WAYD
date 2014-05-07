24d262cdf1b4e7d54840f78e649ea2cf77ad494d
cwlei <cwlei@users.noreply.github.com>
2014-05-06 19:33:02 -0700

    Created welcome/login layout

---
+++ Wayd/WhatAreYouDoingPlanner/src/com/cryinrabbit/whatareyoudoingplanner/LoginFragment.java
@@ -0,0 +1,39 @@
+package com.cryinrabbit.whatareyoudoingplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+	
+	private Button mLoginButton;
+	
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.login_fragment, parent, false);
+		
+		mLoginButton = (Button)v.findViewById(R.id.login_button);
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getActivity(), WaydActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		return v;
+	}			 
+	
+}
